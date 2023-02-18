c61cd728f55fb8ceb45f46c1881083882099bd25
kimhuan <kimhuan@github.com>
2023-02-19 01:53:54 +0700

    first commit

---
+++ HoangKimHuanBTH/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HoangKimHuanBTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
