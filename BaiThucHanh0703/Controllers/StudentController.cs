3be5c4eaf859ef20ce8b0930bd981b37e0c3d7ac
kimhuan <kimhuan@github.com>
2023-03-08 23:33:02 +0700

    Ngay0703

--- BaiThucHanh0703/Controllers/StudentController.cs
+++ BaiThucHanh0703/Controllers/StudentController.cs
@@ -8,6 +8,8 @@ public class StudentController : Controller
 {
     private readonly ILogger<StudentController> _logger;
 
+    public bool StringIsNullOrEmpty { get; private set; }
+
     public StudentController(ILogger<StudentController> logger)
     {
         _logger = logger;
@@ -19,6 +21,15 @@ public class StudentController : Controller
         return View();
     }
 
+    public IActionResult Tinhtong()
+    {
+        return View();
+    }
+    public IActionResult Phuongtrinhbac2()
+    {
+        return View();
+    }
+
     [HttpPost]
     public IActionResult Index(string FullName)
     {
@@ -26,9 +37,50 @@ public class StudentController : Controller
         ViewBag.abc = stdResult;
         return View();
     }
-
-
-
+    [HttpPost]
+    public IActionResult Tinhtong(string Number)
+    {
+        int so = Convert.ToInt32(Number);
+        int tong = 0;
+        while(so > 0 )
+        {
+           tong = tong + so%10;
+           so = so/10;
+        }
+        ViewBag.message = tong;
+        return View();
+    }
+    [HttpPost]
+         public IActionResult Phuongtrinhbac2(string NumA, string NumB, string NumC)
+        {
+            //khai bao bien
+            double delta, x1, x2, a = 0, b = 0, c = 0;
+            string ketqua;
+            //Giai phuong trinh
+            if(!String.IsNullOrEmpty(NumA)) a = Convert.ToDouble(NumA);
+            if(!String.IsNullOrEmpty(NumB)) b = Convert.ToDouble(NumB);
+            if(!String.IsNullOrEmpty(NumC)) c = Convert.ToDouble(NumC);
+            if(a==0) ketqua = "Khong phai phuong trinh bac 2";
+            else{
+                //tinh delta
+                delta = Math.Pow(b,2) - 4*a*c;
+                // Giai phuong trinh
+                if(delta<0) ketqua ="Phuong trinh vo nghiem";
+                else if(delta==0)
+                {
+                    x1 = -b/(2*a);
+                    ketqua ="Phuong trinh co nghiem kep = "+ x1;
+                }
+                else 
+                {
+                    x1= (-b + Math.Sqrt(delta))/(2*a);
+                    x2= (-b - Math.Sqrt(delta))/(2*a);
+                    ketqua = "Phuong trinh co 2 nghiem phan biet: x1 = "+ x1  +", x2 = "+ x2;
+                }
+            }
+            ViewBag.thongbao = ketqua;
+            return View();
+        }
     public IActionResult About()
     {
         return View();
