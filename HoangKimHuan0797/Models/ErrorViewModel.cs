82a6fbf187095fa71023111f99531e0da44ef89c
kimhuan <kimhuan@github.com>
2023-06-22 21:28:16 +0700

    Test

--- HoangKimHuan0797/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace HoangKimHuan0797.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
